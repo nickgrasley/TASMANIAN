61190fa57c0474bb89c422ccb3f199221e09cb48
Nick Grasley <ngrasley@stanford.edu>
2025-06-13 11:15:47 -0700

    Add evaluateBatchGPUd/f to C interface.

--- SparseGrids/TasmanianSparseGridWrapC.cpp
+++ SparseGrids/TasmanianSparseGridWrapC.cpp
@@ -230,6 +230,8 @@ void tsgIntegrate(void *grid, double *q){ ((TasmanianSparseGrid*) grid)->integra
 void tsgDifferentiate(void *grid, const double *x, double *y){ ((TasmanianSparseGrid*) grid)->differentiate(x, y); }
 
 void tsgEvaluateBatch(void *grid, const double *x, int num_x, double *y){ ((TasmanianSparseGrid*) grid)->evaluateBatch(x, num_x, y); }
+void tsgEvaluateBatchGPUd(void *grid, const double *x, int num_x, double *y){ ((TasmanianSparseGrid*) grid)->evaluateBatchGPU(x, num_x, y); }
+void tsgEvaluateBatchGPUf(void *grid, const float *x, int num_x, float *y){ ((TasmanianSparseGrid*) grid)->evaluateBatchGPU(x, num_x, y); }
 
 void tsgBatchGetInterpolationWeightsStatic(void *grid, const double *x, int num_x, double *weights){
     TasmanianSparseGrid* tsg = (TasmanianSparseGrid*) grid;
